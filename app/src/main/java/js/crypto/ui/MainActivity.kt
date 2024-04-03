b294bb48cc0e08386cf6871b98cc14c883b47169
Javad <javacl.sadeghi@gmail.com>
2024-04-03 18:06:45 +0330

    crypto

--- app/src/main/java/js/crypto/ui/MainActivity.kt
+++ app/src/main/java/js/crypto/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package js.crypto
+package js.crypto.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import js.crypto.ui.theme.CryptoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ) {}
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoTheme {
-        Greeting("Android")
     }
 }
